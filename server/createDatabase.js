f3d101919e90eca8b1439f998c541a94694aeada
stephen321 <stephen.enniis@gmail.com>
2019-02-13 03:27:22 +0000

    add createDatabase node file to setup the database  and put db into subdirectory so it will work, permission issue on serve otherwise

---
+++ server/createDatabase.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const sqlite3 = require('sqlite3').verbose()
+
+//-------------------------------------
+// sqlite3 config
+//-------------------------------------
+let db = new sqlite3.Database('database/item_lister.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the SQlite database.');
+      db.run(`CREATE TABLE Items(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name           TEXT    NOT NULL,
+              amount         INT     NOT NULL,
+              path           TEXT    NOT NULL
+           );`, (err) => { if (err) console.log(err)});
+});
